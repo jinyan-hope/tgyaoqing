a83d5e4c714e03cd64aa936c2ea81dae30e30123
jinyan-hope <80030404+jinyan-hope@users.noreply.github.com>
2023-09-20 17:08:42 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,60 @@
+# config.py
+
+# 机器人的TOKEN，替换为你的机器人TOKEN
+BOT_TOKEN = '64552897************'
+
+# 机器人的用户名，替换为你的机器人的用户名
+BOT_USERNAME = "@lim*********t"
+
+# 频道的用户名，替换为你的频道的用户名
+CHANNEL_USERNAME = '@Cn***'
+
+# 管理员的用户ID，替换为你的管理员用户ID
+#ADMIN_USER_ID = 1457254 # 你的管理员用户ID
+
+# 频道链接，替换为你的频道链接
+CHANNEL_LINK = "https:/******"
+
+# 加入频道的提示消息，可以根据需要自定义
+JOIN_CHANNEL_MESSAGE = "请先加入我们的频道以使用此机器人。"
+
+# 预留消息，欢迎用户加入社群
+RESERVED_MESSAGE = "欢迎加入我们的社群！"
+
+# 链接按钮预留信息标题，显示在链接按钮之前
+LINK_BUTTONS_MESSAGE =''' 邀请统计积分机器人\n
+邀请1人获得1积分\n
+15积分可以兑换奖励\n
+详情咨询客服\n'''
+
+# 第一个链接按钮的文本和URL
+LINK_BUTTON_1_TEXT = "加入频道"
+LINK_BUTTON_1_URL = "https://t.**"
+
+# 第二个链接按钮的文本和URL
+LINK_BUTTON_2_TEXT = "联系客服"
+LINK_BUTTON_2_URL = "https://t.me/L*8"
+
+# 兑换积分的值，用户需要达到多少积分才能兑换奖励
+EXCHANGE_SCORE = 15
+
+# 客服链接，用户可以点击以联系客服
+SUPPORT_LINK = "https://t.me/Li*"
+
+HELP_MESSAGE = '''邀请统计机器人\n
+欢迎使用邀请统计机器人！你可以通过邀请好友获取积分，每邀请一个人获得1积分。\n
+积分兑换：\n
+你可以兑换一个月的付费订阅（共享版）需要15积分。\n
+如何获取积分？\n
+点击 '邀请好友' 获取你的邀请链接并分享给你的好友，每个好友加入我们的服务，你将获得1积分。\n
+如何兑换奖励？\n
+当你积累了足够的积分，点击 '兑换奖励' 来兑换你的奖励。\n
+奖励如何使用？\n
+@c*bot。\n
+有问题？联系客服获取帮助。\n'''
+
+# 其他配置项，例如支持的用户名、数据库文件路径、API版本等，根据需要添加
+SUPPORT_USERNAME = "@Li*8"
+USED_KEYS_DB_FILE = 'used_keys.db'
+CHANNEL_USERS_DB_FILE = 'channel_users.db'
+API_VERSION = "13.7"
