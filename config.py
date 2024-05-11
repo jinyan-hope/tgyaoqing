8e420f59942afd708937e75be141ee6433080cd1
jinyan-hope <80030404+jinyan-hope@users.noreply.github.com>
2024-05-11 08:54:34 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,60 +1,64 @@
-# config.py
+# 配置文件，用于你的Telegram机器人
 
-# 机器人的TOKEN，替换为你的机器人TOKEN
-BOT_TOKEN = '64552897************'
+# Telegram Bot API令牌（从BotFather那里获取）
+BOT_TOKEN = '6454541681:AAE58hS1didU'
 
-# 机器人的用户名，替换为你的机器人的用户名
-BOT_USERNAME = "@lim*********t"
+# 你的机器人的用户名
+BOT_USERNAME = 'MTot'
 
-# 频道的用户名，替换为你的频道的用户名
-CHANNEL_USERNAME = '@Cn***'
+# SQLite数据库的文件名
+db_filename = "data.db"
 
-# 管理员的用户ID，替换为你的管理员用户ID
-#ADMIN_USER_ID = 1457254 # 你的管理员用户ID
+# 你的Telegram频道的用户名
+CHANNEL_USERNAME = "@ji11"
 
-# 频道链接，替换为你的频道链接
-CHANNEL_LINK = "https:/******"
+# 你的Telegram频道的链接
+CHANNEL_LINK = "https://t.mn11"
 
-# 加入频道的提示消息，可以根据需要自定义
-JOIN_CHANNEL_MESSAGE = "请先加入我们的频道以使用此机器人。"
+# 兑换奖励所需的积分
+exchange_score = 15
 
-# 预留消息，欢迎用户加入社群
-RESERVED_MESSAGE = "欢迎加入我们的社群！"
+# 客服或联系页面的链接
+SUPPORT_LINK = "https://t.68"
 
-# 链接按钮预留信息标题，显示在链接按钮之前
-LINK_BUTTONS_MESSAGE =''' 邀请统计积分机器人\n
-邀请1人获得1积分\n
-15积分可以兑换奖励\n
-详情咨询客服\n'''
+# 用于管理员功能的Telegram用户ID（作为整数）
+ADMIN_USER_ID = 18  # 用你的管理员用户ID替换
 
-# 第一个链接按钮的文本和URL
+# 按钮1的文本和URL
 LINK_BUTTON_1_TEXT = "加入频道"
-LINK_BUTTON_1_URL = "https://t.**"
+LINK_BUTTON_1_URL = "httpsn11"
 
-# 第二个链接按钮的文本和URL
+# 按钮2的文本和URL
 LINK_BUTTON_2_TEXT = "联系客服"
-LINK_BUTTON_2_URL = "https://t.me/L*8"
+LINK_BUTTON_2_URL = "httme_68"
 
-# 兑换积分的值，用户需要达到多少积分才能兑换奖励
-EXCHANGE_SCORE = 15
+# 保留消息1
+RESERVED_MESSAGE1 = """邀请积分兑换规则
 
-# 客服链接，用户可以点击以联系客服
-SUPPORT_LINK = "https://t.me/Li*"
+邀请1人获得1积分
+15积分可以兑换一月订阅
+详情咨询客服"""
 
+# 帮助消息
 HELP_MESSAGE = '''邀请统计机器人\n
 欢迎使用邀请统计机器人！你可以通过邀请好友获取积分，每邀请一个人获得1积分。\n
 积分兑换：\n
-你可以兑换一个月的付费订阅（共享版）需要15积分。\n
+你可以消耗15积分兑换一个月的付费级别订阅。\n
 如何获取积分？\n
 点击 '邀请好友' 获取你的邀请链接并分享给你的好友，每个好友加入我们的服务，你将获得1积分。\n
 如何兑换奖励？\n
 当你积累了足够的积分，点击 '兑换奖励' 来兑换你的奖励。\n
 奖励如何使用？\n
-@c*bot。\n
+你可以在兑换奖励后按照提示使用奖励。\n
 有问题？联系客服获取帮助。\n'''
 
-# 其他配置项，例如支持的用户名、数据库文件路径、API版本等，根据需要添加
-SUPPORT_USERNAME = "@Li*8"
-USED_KEYS_DB_FILE = 'used_keys.db'
-CHANNEL_USERS_DB_FILE = 'channel_users.db'
-API_VERSION = "13.7"
+# 保留消息2
+RESERVED_MESSAGE2 = """邀请积分兑换规则
+
+邀请1人获得1积分
+15积分可以兑换一月订阅
+详情咨询客服"""
+
+
+EXCHANGE_REWARD_TEXT = "兑换机器人"
+EXCHANGE_REWARD_URL = "https:f_bot"
